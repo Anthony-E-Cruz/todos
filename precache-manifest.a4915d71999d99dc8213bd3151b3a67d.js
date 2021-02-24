e20309bb413a0956f1acbcc8643fd6766c2681f9
Anthony-E-Cruz <anthonyecruz@gmail.com>
2021-02-23 22:27:49 -0500

    Updates

---
+++ precache-manifest.a4915d71999d99dc8213bd3151b3a67d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e387200dad205ffd4105ba631f73b85",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e87ea6d4a56c434b9f92",
+    "url": "./static/css/main.ae0550cc.chunk.css"
+  },
+  {
+    "revision": "5a24ccaca7ad9079738b",
+    "url": "./static/js/2.c9125e2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c9125e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87ea6d4a56c434b9f92",
+    "url": "./static/js/main.f3b955c0.chunk.js"
+  },
+  {
+    "revision": "e71253b17b7b600a2c94",
+    "url": "./static/js/runtime-main.4a2aa1e3.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "./static/media/mintbean.cf18b629.png"
+  }
+]);
